5969c12fc52ebd8697a7e3d81443d51bb2c5eec5
vertex101 <vertex101@gmx.com>
2024-05-31 02:04:07 -0400

    Update StealScythe.java
    fixed MOD_ID

--- src/main/java/net/farzad/stealscythe/StealScythe.java
+++ src/main/java/net/farzad/stealscythe/StealScythe.java
@@ -10,7 +10,7 @@ public class StealScythe implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "stealscythe";
+	public static final String MOD_ID = "steal-scythe";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
